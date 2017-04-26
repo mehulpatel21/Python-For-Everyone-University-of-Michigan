35290b44d9eef1375b86d59ac59685930ff9e9c8
Mehul Patel <mehul.21aug@gmail.com>
2017-04-25 20:43:25 -0400

    chapter 7 completed.

--- Basics/Chapter_7.py
+++ Basics/Chapter_7.py
@@ -1,13 +1,59 @@
-fname = raw_input("Enter the file name: ")
-try:
-    fhand = open(fname)
-except IOError:
-    print "File can't be opened", fname
-    exit()
+def ch7_exe1():
+    fname = raw_input("Enter the file name: ")
+    try:
+        fhand = open(fname)
+    except IOError:
+        print "File can't be opened", fname
+        exit()
 
-for line in fhand:
-    line = line.rstrip()
-    if not line.startswith('From:'):
-        continue
-    print line
+    for line in fhand:
+        line = line.rstrip().upper()
+        print line
 
+
+def ch7_exe2():
+    fname = raw_input("Enter the file name: ")
+    linecount = 0
+    spamconfidence = 0
+    try:
+        fhand = open(fname)
+    except IOError:
+        print "File can't be opened", fname
+        exit()
+    for line in fhand:
+        if not line.startswith('X-DSPAM-Confidence:'):
+            continue
+        colonposition = line.find(':')
+        confstring = line[colonposition+1:]
+        confstring = float(confstring)
+        linecount = linecount + 1
+        spamconfidence = spamconfidence + confstring
+
+    print "Average Spam Confidence: ", spamconfidence/linecount
+
+
+def ch7_exe3():
+    fname = raw_input("Enter the file name: ")
+    count = 0
+    try:
+        if fname == 'na na boo boo':
+            print "NA NA BOO BOO TO YOU - You've been punk'd!"
+            exit()
+
+        fhand = open(fname)
+
+    except IOError:
+        print "File can't be opened", fname
+        exit()
+
+    for line in fhand:
+        if line.startswith('Subject:'):
+            count = count+1
+
+    print "There were", count, " Subject lines in", fname
+
+
+
+ch7_exe1()
+ch7_exe2()
+ch7_exe3()
