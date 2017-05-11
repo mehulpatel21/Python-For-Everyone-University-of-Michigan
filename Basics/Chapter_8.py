1db42aab523f7e92783ab31318e1d6a1be8bf76c
Mehul Patel <mehul.21aug@gmail.com>
2017-05-11 16:20:03 -0400

    chapter 8 completed.

--- Basics/Chapter_8.py
+++ Basics/Chapter_8.py
@@ -11,7 +11,7 @@ def average_of_list():
     print "Average: ", average
 
 
-def extract_dayofthe_week():
+def extract_day_of_the_week():
     fhand = open('mbox-short.txt')
     for line in fhand:
         line = line.rstrip()
@@ -33,4 +33,67 @@ def extract_email_domain():
         print email_domain[1]
 
 
-extract_email_domain()
+#extract_email_domain()
+
+
+def chop(mylist):
+    last_index = len(mylist) - 1
+    del mylist[last_index]
+    del mylist[0]
+    return None
+
+#print chop([1,2,3,4,5])
+
+
+def middle(mylist):
+    return mylist[1:len(mylist)-1]
+
+
+#print middle([1,2,3,4,5])
+
+
+def ch8_exe4():
+    fname = raw_input("Enter file: ")
+    fhand = open(fname)
+    list_of_words = []
+    for line in fhand:
+        line = line.strip('\n').split(' ')
+        for word in line:
+            if word in list_of_words:
+                pass
+            else:
+                list_of_words.append(word)
+    print sorted(list_of_words)
+
+
+def ch8_exe5():
+    fhand = open('mbox-short.txt')
+    count = 0
+    for line in fhand:
+        line = line.rstrip()
+        if not line.startswith('From '):
+            continue
+        line = line.split()
+        email = line[1]
+        count = count + 1
+        print email
+    print "There were", count, "lines in the file with From as the first word."
+
+
+def ch8_exe6():
+    list_of_numbers = []
+    while True:
+        try:
+            user_input = raw_input("Enter a number: ")
+            user_input = float(user_input)
+            if user_input == 'done':
+                raise
+        except:
+            break
+        list_of_numbers.append(user_input)
+    print min(list_of_numbers), max(list_of_numbers)
+
+
+#ch8_exe4()
+#ch8_exe5()
+ch8_exe6()
